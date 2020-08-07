48200ca6d7af596484366f3a4d3f144a1cb886c7
RaselDasel22 <russellmanalac.22@gmail.com>
2020-08-07 12:23:26 +0800

    ok?

---
+++ lesson03exercises/Static Methods Exercises/src/Exercise2_TestInfo.java
@@ -0,0 +1,8 @@
+public class Exercise2_TestInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Calling method from another class:");
+        Exercise1_ParadiseInfo.displayInfo();
+    }
+}
