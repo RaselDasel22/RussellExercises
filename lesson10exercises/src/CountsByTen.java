3cda306b58dae9d3f79251eed3b98a655d61f671
RaselDasel22 <russellmanalac.22@gmail.com>
2020-12-06 23:56:12 +0800

    update

---
+++ lesson10exercises/src/CountsByTen.java
@@ -0,0 +1,20 @@
+public class CountsByTen {
+    public static void main(String[] args) {
+        int countByTen = 10;
+        int newLine=50;
+
+        while (countByTen<=500){
+            while (countByTen<=newLine){
+                if (countByTen<newLine) {
+                    System.out.print(countByTen + ",");
+                }
+                else System.out.print(countByTen);
+                countByTen+=10;
+            }
+            System.out.println();
+            newLine+=50;
+        }
+
+
+    }
+}
---
+++ lesson10exercises/src/Diagonal0s.java
@@ -0,0 +1,12 @@
+public class Diagonal0s {
+    public static void main(String[] args) {
+
+    for(int row =1; row<=10; row++){
+        for(int column =1; column<=row; column++)
+            System.out.print(" ");
+            System.out.println("O");
+
+    }
+
+    }
+}
